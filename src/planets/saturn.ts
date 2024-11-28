1b30348aea6d343fdf012468ebbb98635e469436
nuochka <anstskovalenko@gmail.com>
2024-11-28 21:32:56 +0100

    Ring implementation was refactored to main

--- src/planets/saturn.ts
+++ src/planets/saturn.ts
@@ -1,42 +1,21 @@
 import { Planet } from "../elements/planet";
-import { Ring } from "../elements/ring";
+
+
+export const saturnConfig = {
+    radius: 1.2,
+    latitudeBands: 30,
+    longitudeBands: 30,
+    fieldOfView: 50,
+    aspect: window.innerWidth / window.innerHeight,
+    zNear: 0.1,
+    zFar: 1000.0,
+    textureUrl: 'http://127.0.0.1:8080/textures/saturn_texture.jpg'
+};
 
 export class Saturn extends Planet {
-    private ring: Ring;
-
     constructor(
-        gl: WebGLRenderingContext,
-        x: number = 22.0
-    ) {
-        const saturnConfig = {
-            radius: 1.2,
-            latitudeBands: 30,
-            longitudeBands: 30,
-            fieldOfView: 50,
-            aspect: window.innerWidth / window.innerHeight,
-            zNear: 0.1,
-            zFar: 1000.0,
-            textureUrl: 'http://127.0.0.1:8080/textures/saturn_texture.jpg'
-        };
-        super(gl, saturnConfig, x, -0.002, 0.001, [0.8, 0.8, 0.6, 1.0]);
-        this.ring = new Ring(
-            gl,
-            1.0,
-            2.5,
-            100,
-            'http://127.0.0.1:8080/textures/saturn_ring.png',
-            saturnConfig.fieldOfView,
-            saturnConfig.aspect,
-            saturnConfig.zNear,
-            saturnConfig.zFar,
-            0.001,
-            0.002,
-            x
-        );
-    }
-
-    render(cameraAngleX: number, cameraAngleY: number, cameraDistance: number) {
-        this.ring.render(cameraAngleX, cameraAngleY, cameraDistance);
-        super.render(cameraAngleX, cameraAngleY, cameraDistance);
+        gl: WebGLRenderingContext
+    ) {  
+        super(gl, saturnConfig, 22.0, -0.002, 0.001, [0.8, 0.8, 0.6, 1.0], saturnConfig.radius, "Saturn");
     }
 }
--- src/planets/uranus.ts
+++ src/planets/uranus.ts
@@ -1,45 +1,20 @@
 import { Planet } from "../elements/planet";
-import { Ring } from "../elements/ring";
+
+export  const uranusConfig = {
+    radius: 0.8,
+    latitudeBands: 30,
+    longitudeBands: 30,
+    fieldOfView: 50,
+    aspect: window.innerWidth / window.innerHeight,
+    zNear: 0.1,
+    zFar: 1000.0,
+    textureUrl: 'http://127.0.0.1:8080/textures/uranus_texture.png'
+};
 
 export class Uranus extends Planet {
-    private ring: Ring;
-
     constructor(
-        gl: WebGLRenderingContext,
-        x: number = 40.0
+        gl: WebGLRenderingContext
     ) {
-        const uranusConfig = {
-            radius: 0.8,
-            latitudeBands: 30,
-            longitudeBands: 30,
-            fieldOfView: 50,
-            aspect: window.innerWidth / window.innerHeight,
-            zNear: 0.1,
-            zFar: 1000.0,
-            textureUrl: 'http://127.0.0.1:8080/textures/uranus_texture.png'
-        };
-        super(gl, uranusConfig, x, -0.0004, 0.0005, [0.8, 0.8, 0.6, 1.0]);
-        this.ring = new Ring(
-            gl,
-            1.0,
-            1.5,
-            100,
-            'http://127.0.0.1:8080/textures/uranus_ring.jpg',
-            uranusConfig.fieldOfView,
-            uranusConfig.aspect,
-            uranusConfig.zNear,
-            uranusConfig.zFar,
-            0.0005,
-            0.0004,
-            x,
-            0,
-            0,
-            true
-        );
-    }
-
-    render(cameraAngleX: number, cameraAngleY: number, cameraDistance: number) {
-        this.ring.render(cameraAngleX, cameraAngleY, cameraDistance);
-        super.render(cameraAngleX, cameraAngleY, cameraDistance);
+        super(gl, uranusConfig, 40.0, -0.0004, 0.0005, [0.8, 0.8, 0.6, 1.0], uranusConfig.radius, "Uranus");
     }
 }
