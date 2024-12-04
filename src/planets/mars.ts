f850229a091b536babcf01f845cf1283783383dc
nuochka <anstskovalenko@gmail.com>
2024-12-04 22:39:46 +0100

    Implemented light direction calculations for each planet class

--- src/planets/mars.ts
+++ src/planets/mars.ts
@@ -19,6 +19,9 @@ export class Mars extends Planet{
 }
 
 export class MarsSphere extends Sphere{
+    private orbitRadius: number = 0.0001;
+    private angle: number = 0;
+    private angularSpeed: number = 0;
 
     constructor(gl: WebGLRenderingContext) {
         const marsConfig: SphereConfig = {
@@ -35,7 +38,17 @@ export class MarsSphere extends Sphere{
     }
 
     render(cameraAngleX: number, cameraAngleY: number, cameraDistance: number) {
-        const lightDirection = new Float32Array([1.0, 1.0, 1.0]); 
-        super.render(cameraAngleX, cameraAngleY, cameraDistance, 0, 0, 0, 0, 0, lightDirection, false);
+        this.angle += this.angularSpeed;
+        const x = this.orbitRadius * Math.cos(this.angle);
+        const y = 0;
+        const z = this.orbitRadius * Math.sin(this.angle);
+        const lightDirection = new Float32Array([-x, -y, -z]);
+
+        const length = Math.sqrt(lightDirection[0] ** 2 + lightDirection[1] ** 2 + lightDirection[2] ** 2);
+        lightDirection[0] /= length;
+        lightDirection[1] /= length;
+        lightDirection[2] /= length;
+
+        super.render(cameraAngleX, cameraAngleY, cameraDistance, x, y, z, this.angle, 0, lightDirection, false);
     }
 }
